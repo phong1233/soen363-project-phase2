3c407bbdcbe531103c44b16b53a5c78d310df2ae
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2021-04-12 20:11:02 -0400

    Init nosql

--- mongo/setup.js
+++ mongo/setup.js
@@ -1,5 +1,21 @@
-let res = [
-  db.phase2.drop()
-]
+const { MongoClient } = require("mongodb");
+const csvtojson = require("csvtojson");
 
-printjson(res)
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function loadListings() {
+  const csvData = await csvtojson().fromFile("../dataset/listings.csv");
+  try {
+    await client.connect();
+    const db = client.db("soen363");
+    await db.collection("listings").insertMany(csvData);
+  } finally {
+    client.close();
+  }
+}
+
+loadListings();
