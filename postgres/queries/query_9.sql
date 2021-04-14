1309141955972f145f9ec8912d797efcb73c1915
Sebastien <sebastienbn8@gmail.com>
2021-04-14 14:05:10 -0400

    modified query 9 result

--- postgres/queries/query_9.sql
+++ postgres/queries/query_9.sql
@@ -1,4 +1,4 @@
--- Find top 3 state (province and territory) that are above the canada temperature average
+-- Find top 5 state (province and territory) that are above the canada temperature average in 2010
 SELECT the_state, state_average_temp, canada_average_temp
 FROM (SELECT AVG(average_temp) as canada_average_temp
       FROM globallandtemperaturesbycountry
@@ -11,6 +11,7 @@ FROM (SELECT AVG(average_temp) as canada_average_temp
       GROUP BY the_state) state_temp
 WHERE state_average_temp >= canada_average_temp
 ORDER BY state_average_temp DESC
+LIMIT 5;
 
 -- Result:
 -- |the_state                |state_average_temp|canada_average_temp|
@@ -20,7 +21,4 @@ ORDER BY state_average_temp DESC
 -- |New Brunswick            |6.753083275941511 |-1.8879166555901368|
 -- |Ontario                  |3.119250019391378 |-1.8879166555901368|
 -- |Newfoundland And Labrador|1.9753332634766896|-1.8879166555901368|
--- |British Columbia         |1.6802499492963154|-1.8879166555901368|
--- |Alberta                  |1.5974166095256805|-1.8879166555901368|
--- |Saskatchewan             |1.518166681130727 |-1.8879166555901368|
--- |Manitoba                 |0.4930000305175781|-1.8879166555901368|
+
